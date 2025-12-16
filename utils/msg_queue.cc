701fa43e129dbdd973fddbd0742a3342e7312981
ericmeglovania <1486853661@qq.com>
2025-12-16 12:30:25 +0800

    finish stage3

--- utils/msg_queue.cc
+++ utils/msg_queue.cc
@@ -7,15 +7,20 @@ MSGQueue::~MSGQueue() {}
 // push a ``msg'' to the msg queue
 // we assume that the msg queue has no capacity limit
 void MSGQueue::push_msg(const std::string &msg) {
-    // TODO: your code here
+    std::unique_lock<std::mutex> lock(_mtx);
+    _msg_queue.push(msg);
+    _condition.notify_one();
 }
 
 // pop a msg in the front of msg queue if it is not empty
 // return a std::string variable representing the msg
 std::string MSGQueue::pop_msg() {
-    std::string msg;
-
-    // TODO: your code here
-    
+    std::unique_lock<std::mutex> lock(_mtx);
+	std::string msg;
+    _condition.wait(lock, [&]() -> bool {
+        return !_msg_queue.empty();
+    });
+    msg = _msg_queue.front();
+    _msg_queue.pop();
     return msg;
 }
